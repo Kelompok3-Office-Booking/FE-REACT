aac560022d69d59e8e541b2432112b9e8d853274
mochammadilham06 <stormsykres1006@gmail.com>
2022-11-27 01:11:51 +0700

    feat/ilham : Add Redux configuration (slice, extrareducer, createAsyncThunk)

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const contohSlice = createSlice({
-    name: "contoh",
-    initialState: {
-        contoh: []
+  name: "contoh",
+  initialState: {
+    contoh: [],
+  },
+  reducers: {
+    setContoh: (state, action) => {
+      state.contoh = action.payload;
     },
-    reducers: {
-        setContoh: (state, action) => {
-            state.contoh = action.payload
-        },
-    },
-})
+  },
+});
 
 export const { setContoh } = contohSlice.actions;
 
---
+++ src/store/Feature/FeatureUser/userSlice.js
@@ -0,0 +1,61 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import APIUser from "apis/restApis/User";
+
+const initialState = {
+  data: [],
+  loading: false,
+};
+
+export const fetchUser = createAsyncThunk("fetchUser/users", async () => {
+  try {
+    const res = await APIUser.getAllUsers();
+    return res.data.user;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const updateUser = createAsyncThunk("updateUser/user", async (data) => {
+  try {
+    const res = await APIUser.updateUsers(data);
+    return res.data.update_betterspace_user;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const deleteUser = createAsyncThunk("delete/user", async (id) => {
+  try {
+    const res = await APIUser.deleteUsers(id);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.data = action.payload;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.data = state.data.map((val) => {
+          if (val.id === action.payload.id) {
+            return action.payload;
+          }
+          return val;
+        });
+        state.currentDetail = action.payload;
+        state.loading = false;
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        state.fetchStatus = !state.fetchStatus;
+        state.data = state.data.filter((item) => item.id !== action.payload.id);
+      });
+  },
+});
+
+export default userSlice.reducer;
