c0c9d67cf2c0605691a7226a63b1351b2109a163
mochammadilham06 <stormsykres1006@gmail.com>
2022-11-27 01:09:50 +0700

    feat/ilham : Add Configuration For Users and Transaction APIS

---
+++ src/apis/restApis/User/index.js
@@ -0,0 +1,34 @@
+import axiosInstance from "configs/AxiosInstance";
+
+const APIUser = {
+  async getAllUsers() {
+    try {
+      const response = await axiosInstance.get("/betterspace");
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async updateUsers(data) {
+    try {
+      const id = data.id;
+      const response = await axiosInstance.patch(`/betterspace/${id}`, data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async deleteUsers(id) {
+    try {
+      const response = await axiosInstance.delete(`/betterspace/${id}`);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+export default APIUser;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#020203",
-                bgPrimary: "#18181B",
-                secondary: "#2943A3",
-                third: "##F5F5F5",
-                fourth: "#FCFCFC",
-            },
-            animation: {
-                "bounce-slow": "bounce 1s infinite",
-            },
-        },
-        fontFamily: {
-            sans: ["Inter", "sans-serif"],
-        },
-        container: {
-            padding: "4rem",
-            center: true,
-        },
-        container_mobile: {
-            padding: "2rem",
-            center: true,
-        },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#020203",
+        bgPrimary: "#18181B",
+        secondary: "#2943A3",
+        third: "##F5F5F5",
+        fourth: "#FCFCFC",
+        fifth: "#E9634B",
+      },
+      animation: {
+        "bounce-slow": "bounce 1s infinite",
+      },
     },
-    plugins: [require("flowbite/plugin")],
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    container: {
+      padding: "4rem",
+      center: true,
+    },
+    container_mobile: {
+      padding: "2rem",
+      center: true,
+    },
+  },
+  plugins: [require("flowbite/plugin")],
 };
