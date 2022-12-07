05022b38f36981f020ffce218c80e25737f967ce
mochammadilham06 <stormsykres1006@gmail.com>
2022-12-07 13:26:58 +0700

    feat/ilham : fix users store

--- src/store/Feature/index.js
+++ src/store/Feature/index.js
@@ -3,13 +3,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import users from "./FeatureUser/userSlice";
 import transaction from "./FeatureTransaction/transactionSlice";
 import promo from "./FeaturePromo/promoSlice";
-import contohReducer from "hooks/index";
 import office from "./FeatureOffice/officeSlice";
 import loader from "./FeatureLoader/loaderSlice";
 
 const rootReducers = combineReducers({
   users,
-  data: contohReducer,
   transaction,
   promo,
   office,
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,6 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import persistReducer from "redux-persist/es/persistReducer";
-import persistStore from "redux-persist/es/persistStore";
-import storage from "redux-persist/lib/storage";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducers from "./Feature";
 
-const persistConfig = {
-  key: "contoh",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducers);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer: rootReducers,
 });
-
-export const persistor = persistStore(store);
