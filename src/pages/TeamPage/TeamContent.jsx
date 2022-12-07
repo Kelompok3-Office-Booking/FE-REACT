ca17f525897341fe3c025cb56f403365a79f48a8
David Nasrulloh <davidkrb52@gmail.com>
2022-12-08 03:46:16 +0700

    feat/david: update view team

--- src/pages/TeamPage/TeamContent.jsx
+++ src/pages/TeamPage/TeamContent.jsx
@@ -11,9 +11,9 @@ const TeamContent = () => {
     }, [])
 
     return (
-        <div className="h-full pb-24 pt-20 md:pt-32 container">
+        <div className="h-full pb-32 pt-20 md:pt-32 container">
             <h1 className="pb-12 text-2xl md:text-4xl font-bold title zoomInLeft">Team React <span className="font-normal">- <span className="font-bold">Re</span>mahan Peye(k)<span className="font-bold">act</span> </span></h1>
-            <div className="lg:flex justify-around">
+            <div className="lg:flex justify-around pt-12">
                 <div className="flex flex-col lg:flex-row justify-center lg:pr-10 content-center">
                     <div className="w-full md:w-1/2 my-auto mx-auto">
                         <div className="md:rounded-tr-[125px] rounded-full bg-blue-800 p-3 animate__animated animate__flipInY animate__slow">
@@ -42,52 +42,55 @@ const TeamContent = () => {
                         <h3 className="text-4xl text-blue-700"><span className="font-bold">David</span> Nasrulloh</h3>
                         <hr className="w-1/3 mt-2 border-t-4 border-blue-600" />
                         <h5 className="text-xl py-2">Front End Developer</h5>
-                        <div className="mt-4">
-                            <div className="flex py-3">
+                        <div className="mt-4 flex">
+                            <div className="flex py-3 mx-2">
                                 <a href={`https://github.com/davidnasrulloh`} rel="noreferrer" target="_blank" className="flex">
                                     <div className="mr-2 wave">
                                         <img src={github_team} alt="" className="w-6" />
                                     </div>
-                                    <p>davidnasrulloh</p>
+                                    {/* <p>davidnasrulloh</p> */}
                                 </a>
                             </div>
-                            <div className="flex py-3 md:ml-8">
+                            <div className="flex py-3 mx-2">
                                 <a href={`https://www.instagram.com/davidnasrulloh_`} rel="noreferrer" target="_blank" className="flex">
                                     <div className="mr-2 wave">
                                         <img src={instagram_team} alt="" className="w-6" />
                                     </div>
-                                    <p className="my-auto">davidnasrulloh_</p>
+                                    {/* <p className="my-auto">davidnasrulloh_</p> */}
                                 </a>
                             </div>
-                            <div className="flex py-3 md:ml-16">
+                            <div className="flex py-3 mx-2">
                                 <a href={`https://www.linkedin.com/in/davidnasrulloh/`} rel="noreferrer" target="_blank" className="flex">
                                     <div className="mr-2 wave">
                                         <img src={linkedin_team} alt="" className="w-6" />
                                     </div>
-                                    <p>davidnasrulloh</p>
+                                    {/* <p>davidnasrulloh</p> */}
                                 </a>
                             </div>
-                            <div className="flex py-3 md:ml-24">
+                            <div className="flex py-3 mx-2">
                                 <a href={`https://www.behance.net/davidnasrulloh`} rel="noreferrer" target="_blank" className="flex">
                                     <div className="mr-2 wave">
                                         <img src={behance_team} alt="" className="w-6" />
                                     </div>
-                                    <p>davidnasrulloh</p>
+                                    {/* <p>davidnasrulloh</p> */}
                                 </a>
                             </div>
-                            <div className="flex py-3 md:ml-32">
+                            <div className="flex py-3 mx-2">
                                 <a href={`https://dribbble.com/davidnasrulloh/`} rel="noreferrer" target="_blank" className="flex">
                                     <div className="mr-2 wave">
                                         <img src={dribbble_team} alt="" className="w-6" />
                                     </div>
-                                    <p>davidnasrulloh</p>
+                                    {/* <p>davidnasrulloh</p> */}
                                 </a>
                             </div>
                         </div>
+                        <div className="py-4">
+                            "Kalau yang lain bisa kenapa harus saya ?, karena yang lain tidak bisa seperti saya" hahaha canda
+                        </div>
                     </div>
                 </div>
-                <div className="invisible lg:visible flex my-auto"><hr className="h-0 lg:h-96 border-r-2" /></div>
-                <div className="flex flex-col lg:flex-row justify-center content-center">
+                <div className="invisible lg:visible flex my-auto"><hr className="h-0 lg:h-96 border-r-2 animate__fadeInDown" /></div>
+                <div className="flex flex-col lg:flex-row justify-center content-center pl-12">
                     <div className="animate__animated animate__backInUp animate__slow w-full lg:w-1/2 py-12 md:pr-10 flex text-end flex-col justify-start text-blue-800">
                         <div className="text-xl flex justify-end">
                             <Typed
@@ -110,31 +113,34 @@ const TeamContent = () => {
                             <hr className="w-1/3 mt-2 border-t-4 border-blue-600" />
                         </div>
                         <h5 className="text-xl py-2">Front End Developer</h5>
-                        <div className="mt-4 flex flex-col justify-end">
-                            <div className="flex py-3 justify-end">
+                        <div className="mt-4 flex justify-end">
+                            <div className="flex py-3 justify-end mx-2">
                                 <a href={`https://github.com/mochammadilham06`} rel="noreferrer" target="_blank" className="flex">
-                                    <p>mochammadilham06</p>
+                                    {/* <p>mochammadilham06</p> */}
                                     <div className="ml-2 wave">
                                         <img src={github_team} alt="" className="w-6" />
                                     </div>
                                 </a>
                             </div>
-                            <div className="flex py-3 justify-end md:mr-8">
+                            <div className="flex py-3 justify-end mx-2">
                                 <a href={`https://www.instagram.com/moch.ilham10`} rel="noreferrer" target="_blank" className="flex">
-                                    <p className="my-auto">moch.ilham10</p>
+                                    {/* <p className="my-auto">moch.ilham10</p> */}
                                     <div className="ml-2 wave">
                                         <img src={instagram_team} alt="" className="w-6" />
                                     </div>
                                 </a>
                             </div>
-                            <div className="flex py-3 justify-end md:mr-16">
+                            <div className="flex py-3 justify-end mx-2">
                                 <a href={`https://www.linkedin.com/in/mochilham1006`} rel="noreferrer" target="_blank" className="flex">
-                                    <p>mochilham1006</p>
+                                    {/* <p>mochilham1006</p> */}
                                     <div className="ml-2 wave">
                                         <img src={linkedin_team} alt="" className="w-6" />
                                     </div>
                                 </a>
                             </div>
+                        </div>
+                        <div className="py-4">
+                            "Hidup itu sederhana, goreng angkat, lalu tiriskan"
                         </div>
                     </div>
                     <div className="w-full md:w-1/2 my-auto mx-auto">
