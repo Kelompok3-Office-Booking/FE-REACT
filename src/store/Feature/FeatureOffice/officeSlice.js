4539891563a0b050664d41186c98b960e3afa081
mochammadilham06 <stormsykres1006@gmail.com>
2022-12-15 11:17:30 +0700

    feat/ilham : setting api and redux for offices

--- src/store/Feature/FeatureOffice/officeSlice.js
+++ src/store/Feature/FeatureOffice/officeSlice.js
@@ -5,32 +5,33 @@ import {
 } from "@reduxjs/toolkit";
 import APIOffice from "apis/restApis/Office";
 
-// const initialState = {
-//     data: [],
-//     loading: false,
-//     status : "idle",
-//     error :
-// };
+const initialState = {
+  data: [],
+  loading: false,
+  status: "idle",
+  error: null,
+};
 
 export const fetchOffice = createAsyncThunk("fetch/office", async () => {
   try {
     const res = await APIOffice.getAllOffice();
-    console.log(res.data.data);
     return res.data.data;
   } catch (err) {
     console.log(err);
   }
 });
 
-export const createOffice = createAsyncThunk("create/office", async (data) => {
-  try {
-    const res = await APIOffice.createOffice();
-    console.log(res.data);
-    return res.data;
-  } catch (err) {
-    console.log(err);
+export const createOffice = createAsyncThunk(
+  "create/office",
+  async (formData) => {
+    try {
+      const res = await APIOffice.createOffice(formData);
+      return res;
+    } catch (err) {
+      console.log(err);
+    }
   }
-});
+);
 
 export const updateOffice = createAsyncThunk("update/office", async (data) => {
   try {
@@ -55,45 +56,47 @@ export const deleteOffice = createAsyncThunk("delete/office", async (id) => {
 const OfficesEntity = createEntityAdapter({
   selectId: (offices) => offices.id,
 });
-console.log(OfficesEntity.getInitialState());
 
 const officeSlice = createSlice({
   name: "office",
-  initialState: OfficesEntity.getInitialState(),
-  extraReducers: {
-    [fetchOffice.pending]: (state, action) => {
-      state.status = "Pending";
-    },
-    [fetchOffice.fulfilled]: (state, action) => {
-      state.status = "Succeeded";
-      OfficesEntity.setAll(state, action.payload);
-    },
-    [fetchOffice.rejected]: (state, action) => {
-      state.status = "Rejected";
-      state.error = "Error";
-    },
-    [createOffice.pending]: (state, action) => {
-      state.status = "Pending";
-    },
-    [createOffice.fulfilled]: (state, action) => {
-      state.status = "Succeeded";
-      OfficesEntity.addOne(state, action.payload);
-    },
-    [createOffice.rejected]: (state, action) => {
-      state.status = "Rejected";
-      state.error = "Error";
-    },
-    [deleteOffice.pending]: (state, action) => {
-      state.status = "Pending";
-    },
-    [deleteOffice.fulfilled]: (state, action) => {
-      state.status = "Succeeded";
-      OfficesEntity.removeOne(state, action.payload);
-    },
-    [deleteOffice.rejected]: (state, action) => {
-      state.status = "Rejected";
-      state.error = "Error";
-    },
+  initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(fetchOffice.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchOffice.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.data = action.payload;
+      })
+      .addCase(fetchOffice.rejected, (state, action) => {
+        state.status = "failed";
+      })
+      .addCase(updateOffice.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(updateOffice.fulfilled, (state, action) => {
+        state.data = state.data.map((val) => {
+          if (val.id === action.payload.id) {
+            return action.payload;
+          }
+          return val;
+        });
+        state.loading = false;
+      })
+      .addCase(updateOffice.rejected, (state, action) => {
+        state.status = "failed";
+      })
+      .addCase(deleteOffice.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(deleteOffice.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.data = state.data.filter((item) => item.id !== action.payload.id);
+      })
+      .addCase(deleteOffice.rejected, (state, action) => {
+        state.status = "failed";
+      });
   },
 });
 // const officeSlice = createSlice({
@@ -121,8 +124,8 @@ const officeSlice = createSlice({
 //   },
 // });
 
-export const OfficesSelectors = OfficesEntity.getSelectors(
-  (state) => state.office
-);
+// export const OfficesSelectors = OfficesEntity.getSelectors(
+//   (state) => state.office
+// );
 
 export default officeSlice.reducer;
