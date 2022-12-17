ea695a1c911ac3467e52731182508883c6e9b22f
mochammadilham06 <stormsykres1006@gmail.com>
2022-12-17 21:13:32 +0700

    feat/ilham : fix display offices

--- src/components/LayoutAdmin/office/index.jsx
+++ src/components/LayoutAdmin/office/index.jsx
@@ -280,7 +280,7 @@ const OfficePage = () => {
                   id="table-search-users"
                   onChange={(ev) => handleSearch(ev)}
                   className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="Search for users"
+                  placeholder="Search for offices"
                 />
               </div>
             </div>
